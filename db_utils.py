0d27f04d0b3d6bdf119b4dc77f29fea1e0e7bde0
Chris <chills60@gmail.com>
2024-06-06 19:43:42 +0100

    Updates db_utils.py with code for task 2, successfully connects to AWS db and creates df of table.

--- db_utils.py
+++ db_utils.py
@@ -1,6 +1,36 @@
-class RDSDatabaseConnector():
-    def__init__(self):
-        pass
+import yaml
+from sqlalchemy import create_engine
+import pandas as pd
+import psycopg2
 
-    def load_rds():
-        pass
+# Function to load credentials.yaml file and return data as a dictionary:
+def load_credentials(file_path):
+    '''
+    Function to load access credentials to AWS remote database from secure yaml file. 
+    Returns credentials in dictionary format.
+    '''
+    with open(file_path, 'r') as file:
+        credentials = yaml.safe_load(file)
+    return credentials
+
+# Class to extract data from remote AWS database:
+class RDSDatabaseConnector:
+    def __init__(self, credentials):
+        self.credentials = credentials
+
+    def connect_db(self, DATABASE_TYPE, DBAPI, RDS_USER, RDS_PASSWORD, RDS_HOST, RDS_PORT, RDS_DATABASE):
+        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}")
+        conn = engine.connect()
+        df = pd.read_sql(f'SELECT * FROM {table_name}', conn)
+        print(df)
+
+def load_db(credentials_file_path):
+    credentials = load_credentials(credentials_file_path)
+    connection = RDSDatabaseConnector(credentials)
+    connection.connect_db(**credentials)    # Use of the unpacking operator (**) to unpack dictionary into keyword arguments of a function. 
+
+if __name__ == '__main__':
+    credentials_file_path = 'C:/Users/Chris/Documents/Career development/AiCore Bootcamp/EDA-Project/credentials.yaml' # Use of forward slash instead of backslash
+    table_name = 'customer_activity'
+    load_db(credentials_file_path)
+
