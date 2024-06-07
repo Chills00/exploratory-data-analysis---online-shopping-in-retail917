d9da4822c599b1e44a76a67415ca3f9184443739
Chris <chills60@gmail.com>
2024-06-07 12:51:14 +0100

    Updates README.md file, adds docstrings to db_utils.pp.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # exploratory-data-analysis---online-shopping-in-retail917
+## Description
+This Exploratory Data Analysis (EDA) project was part of AiCore's training for their Data Analysis course. The purpose is to provide real-world experience of data analysis by exploring a dataset of online shopping website activity. 
+
+The project will showcase the power of EDA in uncovering valuable insights from large datasets and how these insights can be leveraged to drive business success. 
+
+The data contains information about the website activity of users over one year. Each sample represents the user interacting with the website during a shopping session. 
+
+A variety of statistical and visualisation techniques will be used to explore the data. The analysis will provide a better understanding of customer behaviour but also help the business optimise marketing strategies and improve their customer experience.
+
+The project is split across 4 key milestones:
+1. Setting up the environment. 
+1. Extracting the online shopping data from the cloud.
+1. Exploratory Data Analysis (EDA).
+1. Analysis and Visualisation.
+
+### What did I learn:
+Below are examples of how this project has supported my learning:
+1. Milestone 1:
+    - How to set up a dev environment. 
+    - How to set up and use git and GitHub repo for version control. 
+1. Milestone 2:
+    - The PyYAML package safe_load function can be used to securely load security credentials, returning them as a dictionary. 
+    - The unpacking operator (**) can be used to unpack a dictionary into keyword arguments of a function. This was useful for creating the database URL.
+    - How to use the sqlalchemy create_engine function to create a Dialect object tailored towards PostgreSQL, as well as a Pool object which establishes a DBAPI connection.
+1. Milestone 3:
+
+
+## Table of Contents
+
+## Usage
+
+## Licence
+
--- db_utils.py
+++ db_utils.py
@@ -8,6 +8,9 @@ def load_credentials(file_path):
     '''
     Function to load access credentials to AWS remote database from secure yaml file. 
     Returns credentials in dictionary format.
+
+    Parameters:
+        file_path (str): File path for local .yaml file containing login credentials for AWS RDS. 
     '''
     with open(file_path, 'r') as file:
         credentials = yaml.safe_load(file)
@@ -15,10 +18,36 @@ def load_credentials(file_path):
 
 # Class to extract data from remote AWS database:
 class RDSDatabaseConnector:
+    '''
+    This class is used to connect to a remote AWS database and retrieve tabular data.
+
+    ------------------
+    Parameters:
+    credentials: dict
+        Dictionary of credentials required to connect to remote RDS.
+
+    ------------------
+    Attributes:
+    credentials: dict
+        Dictionary of credentials required to connect to remote RDS.
+
+    ------------------
+    Methods:
+    save_data(df)
+        Saves df created as .csv file to local directory.
+    connect_db(DATABASE_TYPE, DBAPI, RDS_USER, RDS_PASSWORD, RDS_HOST, RDS_PORT, RDS_DATABASE)
+        Connects to remote RDS and creates Pandas df from specified table name.
+    '''
     def __init__(self, credentials):
         self.credentials = credentials
     
     def save_data(self, df):
+        '''
+        This method saves the Pandas df to .csv file in specified local directory.
+
+        Parameters:
+            df: Pandas DataFrame.
+        '''
         try:
             df.to_csv(f'{directory}{table_name}.csv', index=False)
             print('Data saved')
@@ -26,6 +55,18 @@ class RDSDatabaseConnector:
             print('Saving failed')
     
     def connect_db(self, DATABASE_TYPE, DBAPI, RDS_USER, RDS_PASSWORD, RDS_HOST, RDS_PORT, RDS_DATABASE):
+        '''
+        This method connects to the remote RDS and creates a Pandas df of all data from specefied table name.
+
+        Parameters:
+            DATABASE_TYPE (str): dialect
+            DBAPI (str): driver
+            RDS_USER (str): username
+            RDS_PASSWORD (str): password
+            RDS_HOST (str): host
+            RDS_PORT (str): port
+            RDS_DATABASE (str): database name
+        '''
         try:
             engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}")
             conn = engine.connect()
@@ -37,13 +78,19 @@ class RDSDatabaseConnector:
         return self.save_data(df)
         
 def load_db(credentials_file_path):
+    '''
+    This function creates an instance of the class RDSDatabaseConnector and calls the connect_db function to load db using login credentials.
+
+    Parameters:
+        credentials_file_path (str): Local file path for .yaml file containing login credentials.
+    '''
     credentials = load_credentials(credentials_file_path)
     connection = RDSDatabaseConnector(credentials)
     connection.connect_db(**credentials)    # Use of the unpacking operator (**) to unpack dictionary into keyword arguments of a function. 
 
 if __name__ == '__main__':
     credentials_file_path = 'C:/Users/Chris/Documents/Career development/AiCore Bootcamp/EDA-Project/credentials.yaml' # Use of forward slash instead of backslash
-    table_name = 'customer_activity'
+    table_name = 'customer_activity'    # Name of table in db, also used to create file_name.
     directory = 'C:/Users/Chris/Documents/Career development/AiCore Bootcamp/EDA-Project/'  # To save df
     load_db(credentials_file_path)
 
