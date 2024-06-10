185463ab82675fe4d9e4380aafdb458de9d1ef38
Chris <chills60@gmail.com>
2024-06-10 15:29:24 +0100

    Adds docstrings to df_info.py and transformations.py files.

--- df_info.py
+++ df_info.py
@@ -1,16 +1,69 @@
 class DataFrameInfo():
+    '''
+    This class is used to generate basic information about a dataframe.
+
+    ------------------
+    Parameters:
+    df_name: Pandas Dataframe
+        Dataset in Pandas Dataframe.
+
+    ------------------
+    Attributes:
+    df_name: Pandas Dataframe
+        Dataset in Pandas Dataframe.
+
+    ------------------
+    Methods:
+    df_data_type()
+        Returns the data type of each column.
+    df_stats()
+        Returns the descriptive statistics for the df.
+    df_category_distinct_values()
+        Prints the value counts of unique values for each column containing categorical data.
+    df_shape()
+        Prints the shape of the df.
+    df_null_info()
+        Prints the number of null values for each column as a percentage of all values.
+    '''
     def __init__(self, df_name):
         self.df_name = df_name
 
     def df_data_type(self):
+        '''
+        This method returns the data type of each column.
+
+        Parameters:
+            df_name: Pandas Dataframe
+        
+        Returns:
+            The data type of each column.
+        '''
         print('The data type for each column is listed below:')
         return self.df_name.dtypes
 
     def df_stats(self):
+        '''
+        This method returns the descriptive statistics for the df.
+
+        Parameters:
+            df_name: Pandas Dataframe
+        
+        Returns:
+            The statistical summary for each column containing numerical data.
+        '''
         print('A statistical summary of the numerical columns is below:')
         return self.df_name.describe()
 
     def df_category_distinct_values(self):
+        '''
+        This method prints the value counts of unique values for each column containing categorical data.
+
+        Parameters:
+            df_name: Pandas Dataframe
+        
+        Returns:
+            The value counts of unique values for each column containing categorical data.
+        '''
         print('The frequency of unique values for each column containing categorical data is below:')
         print()
         data_type_list = self.df_name.dtypes
@@ -22,10 +75,28 @@ class DataFrameInfo():
                 continue
         
     def df_shape(self):
+        '''
+        This method prints shape of the df.
+
+        Parameters:
+            df_name: Pandas Dataframe
+        
+        Returns:
+            The shape of the df, providing the number of columns and number of rows that the dataset has.
+        '''
         shape = self.df_name.shape
         return f'The dataset has {shape[0]} columns and {shape[1]} rows.'
 
     def df_null_info(self):
+        '''
+        This method prints the number of null values for each column as a percentage of all values.
+
+        Parameters:
+            df_name: Pandas Dataframe
+        
+        Returns:
+            The number of null values for each column as a percentage of all values.
+        '''
         all_values = len(self.df_name)
         for column in list(self.df_name):
             sum_missing_values = self.df_name[column].isna().sum()
--- transformations.py
+++ transformations.py
@@ -1,22 +1,79 @@
 class DataTransform():
     '''
-    
+    This class is used to change the data type of specified column(s) to a new data type.
+
+    ------------------
+    Parameters:
+    *args: Column header(s)
+        Any number of columns
+
+    ------------------
+    Attributes:
+    *args: Column header(s)
+        Any number of columns
+
+    ------------------
+    Methods:
+    to_category()
+        Converts the specified column(s) to category data type.
+    to_datetime()
+        Converts the specified column(s) to datetime64 data type.
+    to_Int()
+        Converts the specified column(s) to Int64 data type.
+    to_int()
+        Converts the specified column(s) to int64 data type.
     '''
     def __init__(self, df_name):
         self.df_name = df_name
 
     def to_category(self, *args):
+        '''
+        This method converts the specified column(s) to category data type.
+
+        Parameters:
+            *args (str): Column header(s)
+        
+        Returns:
+            The specified column(s) as category.
+        '''  
         for arg in args:
             self.df_name[arg] = self.df_name[arg].astype('category')
 
     def to_datetime(self, *args):
+        '''
+        This method converts the specified column(s) to datetime64 data type.
+
+        Parameters:
+            *args (str): Column header(s)
+        
+        Returns:
+            The specified column(s) as datetime64 to the millisecond value.
+        '''  
         for arg in args:
             self.df_name[arg] = self.df_name[arg].astype('timedelta64[ms]')
 
     def to_Int(self, *args):
+        '''
+        This method converts the specified column(s) to Int64 data type.
+
+        Parameters:
+            *args (str): Column header(s)
+        
+        Returns:
+            The specified column(s) as Int64.
+        '''  
         for arg in args:
             self.df_name[arg] = self.df_name[arg].astype('Int64')
 
     def to_int(self, *args):
-            for arg in args:
-                self.df_name[arg] = self.df_name[arg].astype('int64')
+        '''
+        This method converts the specified column(s) to int64 data type.
+
+        Parameters:
+            *args (str): Column header(s)
+        
+        Returns:
+            The specified column(s) as int64.
+        '''    
+        for arg in args:
+            self.df_name[arg] = self.df_name[arg].astype('int64')
